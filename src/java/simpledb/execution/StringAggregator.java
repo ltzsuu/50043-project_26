98251e673cd06e94f57d044cf53225d8c3310a5b
zektron001 <95159263+zektron001@users.noreply.github.com>
2026-07-10 16:38:50 +0800

    REFACTOR: Implement String Aggregator

--- src/java/simpledb/execution/StringAggregator.java
+++ src/java/simpledb/execution/StringAggregator.java
@@ -9,7 +9,12 @@ import simpledb.storage.Tuple;
 public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
-
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
+    boolean hasGroupings = false;
+    private Map<Field, Integer> aggregatedResults;
     /**
      * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
@@ -21,6 +26,15 @@ public class StringAggregator implements Aggregator {
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.what = what;
+        hasGroupings = (gbfield != NO_GROUPING);
+        if (what != Op.COUNT) {
+            throw new IllegalArgumentException("StringAggregator only supports COUNT");
+        }
+        this.aggregatedResults = new HashMap<>();
     }
 
     /**
@@ -29,6 +43,13 @@ public class StringAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+         Field fieldToGroup = NO_GROUPING_FIELD;
+        if (hasGroupings) {
+            fieldToGroup = tup.getField(gbfield);
+        }
+        String aggregateVal = tup.getField(afield).toString();
+        int count = aggregatedResults.getOrDefault(fieldToGroup, 0);
+        aggregatedResults.put(fieldToGroup, count + 1);
     }
 
     /**
@@ -41,7 +62,23 @@ public class StringAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
-    }
+        TupleDesc td;
+        if (hasGroupings) {
+            td = new TupleDesc(new Type[]{gbfieldtype, Type.INT_TYPE});
+        } else {
+            td = new TupleDesc(new Type[]{Type.INT_TYPE});
+        }
 
-}
+        List<Tuple> tuples = new ArrayList<>();
+        for (Map.Entry<Field, Integer> entry : aggregatedResults.entrySet()) {
+            Tuple t = new Tuple(td);
+            if (hasGroupings) {
+                t.setField(0, entry.getKey());
+                t.setField(1, new IntField(entry.getValue()));
+            } else {
+                t.setField(0, new IntField(entry.getValue()));
+            }
+            tuples.add(t);
+        }
+        return new TupleIterator(td, tuples);
+    }
