f3f94d01b66429cc45fd10999eed346f9c55f01e
zektron001 <95159263+zektron001@users.noreply.github.com>
2026-07-10 15:15:54 +0800

    Update Predicate.java
    implement the predicate function

--- src/java/simpledb/execution/Predicate.java
+++ src/java/simpledb/execution/Predicate.java
@@ -58,7 +58,9 @@ public class Predicate implements Serializable {
      *            field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // some code goes here
+         this.field = field;
+         this.op = op;
+         this.operand = operand;
     }
 
     /**
@@ -66,8 +68,7 @@ public class Predicate implements Serializable {
      */
     public int getField()
     {
-        // some code goes here
-        return -1;
+        return field;
     }
 
     /**
@@ -75,8 +76,7 @@ public class Predicate implements Serializable {
      */
     public Op getOp()
     {
-        // some code goes here
-        return null;
+        return op;
     }
     
     /**
@@ -84,8 +84,7 @@ public class Predicate implements Serializable {
      */
     public Field getOperand()
     {
-        // some code goes here
-        return null;
+        return operand;
     }
     
     /**
@@ -99,8 +98,7 @@ public class Predicate implements Serializable {
      * @return true if the comparison is true, false otherwise.
      */
     public boolean filter(Tuple t) {
-        // some code goes here
-        return false;
+        return t.getField(field).compare(op, operand);
     }
 
     /**
@@ -109,6 +107,6 @@ public class Predicate implements Serializable {
      */
     public String toString() {
         // some code goes here
-        return "";
+        return "Field: " + field + ", Operator: " + op + ", Operand: " + operand;
     }
 }
