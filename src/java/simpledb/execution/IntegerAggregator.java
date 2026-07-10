f5cca855c118e3c4d7dd98bff1a814f7667327c2
zektron001 <95159263+zektron001@users.noreply.github.com>
2026-07-10 16:00:50 +0800

    refactor: IntegerAggregator

--- src/java/simpledb/execution/IntegerAggregator.java
+++ src/java/simpledb/execution/IntegerAggregator.java
@@ -9,7 +9,10 @@ import simpledb.storage.Tuple;
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
-
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
     /**
      * Aggregate constructor
      * 
@@ -27,6 +30,10 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.what = what;
     }
 
     /**
